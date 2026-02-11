ffabbef80351dce2fe31fedb3d4a33bee2a0b134
Mohamed ali Bettaieb <medali.bettaiebrj@gmail.com>
2026-02-11 13:35:33 +0100

    Update Firebase configuration to use placeholders
    Replaced existing Firebase configuration with placeholders for user input.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAwj2A5Q727pfruzVz7tcBmh7W4WN3cBBU",
-    authDomain: "gestion-films-6825c.firebaseapp.com",
-    projectId: "gestion-films-6825c",
-    storageBucket: "gestion-films-6825c.appspot.com",
-    messagingSenderId: "615573341599",
-    appId: "1:615573341599:web:80ee56bf73effce983e9aa"
-  }
+//Coller votre configuration ici
+export const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "votre-projet.firebaseapp.com",
+  projectId: "votre-projet",
+  storageBucket: "votre-projet.appspot.com",
+  messagingSenderId: "VOTRE_SENDER_ID",
+  appId: "VOTRE_APP_ID"
 };
